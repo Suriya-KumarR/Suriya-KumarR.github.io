86028f7e393eb581fa56a4fd28a7bd9839968d1a
Suriya-KumarR <suriya.kumarrr2002@gmail.com>
2024-11-08 10:32:10 -0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SplineScene from './SplineScene';
+
+function App() {
+  return (
+    <div className="App">
+      <SplineScene />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/SplineScene.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Spline from '@splinetool/react-spline';
+
+export default function SplineScene() {
+  return (
+    <main style={{ width: '100%', height: '100vh' }}>
+      <Spline scene="https://prod.spline.design/kaqGYIKhk3FIDEvL/scene.splinecode" />
+    </main>
+  );
+}
